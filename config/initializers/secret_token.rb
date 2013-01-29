c7b283d42bbe9d396e8d2e68a5dbabdcaee471a8
woolen <just_woolen@qq.com>
2013-01-29 23:54:45 +0800

    creat proj
    建立初始的项目相关文件

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsWeb::Application.config.secret_token = '05c41dcdc34f9538cc35a76829c77b03a8b4cb0fa24366f59560f9cde13374f6f49ed188ba0308518bf1730d73fba78e6d5db39ef92a8b7772d60cbf27549a42'
